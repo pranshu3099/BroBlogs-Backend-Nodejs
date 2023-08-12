33596172cff28ff132dcf83e94ee55d2bbed5511
pranshu <srivastavapranshu2@gmail.com>
2023-08-12 23:26:37 +0530

    added to try catch

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -2,8 +2,14 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getCategories = async (req, res) => {
-  const allCategories = await prisma.categories.findMany();
-  return res.status(200).json({ categories: allCategories });
+  try {
+    const allCategories = await prisma.categories.findMany();
+    return res.status(200).json({ categories: allCategories });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: err });
+  }
 };
 
 module.exports = {
